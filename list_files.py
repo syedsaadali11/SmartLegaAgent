664bbdeb750ca9dfaa633460f13b4f88190ff5a2
syedsaadali11 <134360720+syedsaadali11@users.noreply.github.com>
2025-06-30 21:35:06 +0500

    Removed list_files.py from Git tracking and added to .gitignore

--- list_files.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def list_top_folders(path="."):
-    print("📁 Top-level folders in your project:\n")
-    for item in os.listdir(path):
-        if os.path.isdir(item):
-            print(f" - {item}/")
-
-list_top_folders()
